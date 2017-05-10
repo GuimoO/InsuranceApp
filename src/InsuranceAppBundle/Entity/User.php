71e8d760d2d3af9ff6cdea1242dc578ae73c5d63
GuimoO <guiguismoo@gmail.com>
2017-05-10 23:51:45 +0200

    install bundle fosuser + config + creation bdd + maj schema

---
+++ src/InsuranceAppBundle/Entity/User.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: wcs-fontainebleau
+ * Date: 10/05/17
+ * Time: 22:10
+ */
+
+namespace InsuranceAppBundle\Entity;
+
+use FOS\UserBundle\Model\User as BaseUser;
+
+
+
+class User extends BaseUser
+
+{
+    protected $id;
+
+
+}
